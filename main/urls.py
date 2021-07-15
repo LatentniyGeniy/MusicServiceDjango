6ac139d6fdc533083b79acc809bbf8ff7902f815
LatentniyGeniy <82951021+LatentniyGeniy@users.noreply.github.com>
2021-07-15 22:11:21 +0300

    serializers (#2)
    * add-rest-framework
    
    * add README
    
    * fixed README and .gitignore
    
    * serializers

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('craeate', views.create, name='create')
+#    path('', views.index, name='home'),
+#    path('craeate', views.create, name='create'),
+    path('album/', views.AlbumListView.as_view()),
+    path('create-album/', views.AlbumCreateView.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -1,32 +1,99 @@
-from django.shortcuts import render, redirect
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .forms import AlbumForm
+
 from .models import Album
+from .serializers import AlbumListSerializer, AlbumCreateSerializer
 
 
-def index(request):
-    albums = Album.objects.all()
-    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'albums': albums})
+class AlbumListView(APIView):
+    def get(self, request):
+        albums = Album.objects.all()
+        serializer = AlbumListSerializer(albums, many=True)
+        return Response(serializer.data)
 
 
-def create(request):
-    error = ''
-    if request.method == 'POST':
-        form = AlbumForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-        else:
-            error = 'Форма неверна'
-
-    form = AlbumForm()
-    context = {
-        'form': form,
-        'error': error
-    }
-    return render(request, 'main/create.html', context)
+class AlbumCreateView(APIView):
+    def post(self, request):
+        album = AlbumCreateSerializer(data=request.data)
+        if album.is_valid():
+            album.save()
+        return Response(status=201)
 
 
-def edit(request, id):
-    album = Album.objects.get(id=id)
-    return render(request, 'main/create.html', {'album': album})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.shortcuts import render, redirect
+#
+# from .forms import AlbumForm
+# from .models import Album
+#
+#
+# def index(request):
+#     albums = Album.objects.all()
+#     return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'albums': albums})
+#
+#
+# def create(request):
+#     error = ''
+#     if request.method == 'POST':
+#         form = AlbumForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#         else:
+#             error = 'Форма неверна'
+#
+#     form = AlbumForm()
+#     context = {
+#         'form': form,
+#         'error': error
+#     }
+#     return render(request, 'main/create.html', context)
+#
+#
+# def edit(request, id):
+#     album = Album.objects.get(id=id)
+#     return render(request, 'main/create.html', {'album': album})
