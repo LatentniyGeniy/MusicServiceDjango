3fda4d508082273e9aedb3421ec32330a07af1ce
LatentniyGeniy <vizzza99@gmail.com>
2021-07-30 20:26:06 +0300

    add tests-update/create

--- tests/api/test_song.py
+++ tests/api/test_song.py
@@ -17,13 +17,14 @@ class TestSong:
         assert res.status_code == status.HTTP_200_OK
         assert len(res.json()) == songs_qty
 
-    @pytest.mark.parametrize('genres_qty', [3])
+    @pytest.mark.parametrize('genres_qty', [5])
     def test_create_song(self, client, album, genres, genres_qty):
         data = {
             'title': 'Some title',
             'genre': [genre.id for genre in genres],
             'album': album.id,
-            'file_link': 'https://file/asdasda.mp3'
+            'file_link': 'https://file/asdasda.mp3',
+
         }
         res = client.post(f'/api/v1/songs/', data=data, content_type='application/json')
         response_data = res.json()
@@ -33,6 +34,22 @@ class TestSong:
         assert len(response_data['genre']) == len(data['genre'])
         assert response_data['album'] == data['album']
 
+    def test_update_song(self, client, song):
+        data = {
+            'title': 'Edited title',
+        }
+        res = client.patch(f'/api/v1/songs/{song.id}/', data=data, content_type='application/json')
+        response_data = res.json()
+
+        assert res.status_code == status.HTTP_200_OK
+        assert response_data['title'] == data['title']
+
+    def test_detail(self, client, song):
+        res = client.get(f'/api/v1/songs/{song.id}/')
+        response_data = res.json()
+
+        assert res.status_code == status.HTTP_200_OK
+        assert response_data['title'] == song.title
 
 # GET /api/v1/songs/ - GET LIST
 # POST /api/v1/songs/ - CREATE NEW SONG
